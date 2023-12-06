3d7beb298b1c6d07790434b0b6bca9ab1bfced85
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-12-06 12:35:59 -0600

    RESTART PROJECT FROM SCRATCH

--- my-website/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Proposterious Domain';
-export const SITE_DESCRIPTION = 'Welcome Home to All Things "Proposterious"';
