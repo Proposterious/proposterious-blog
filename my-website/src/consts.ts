2b64a4978d6a244d80088001ca2dca37d58c6f12
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-12-06 00:21:13 -0600

    Edit site description

--- my-website/src/consts.ts
+++ my-website/src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Proposterious Domain';
-export const SITE_DESCRIPTION = 'Welcome to home of all things "proposterious"';
+export const SITE_DESCRIPTION = 'Welcome Home to All Things "Proposterious"';
